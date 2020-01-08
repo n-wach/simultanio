bceb0bf38584072fee62a945244df6ce694dbb61
xdrie <xdrie@alt.icu>
2020-01-08 15:14:34 -0800

    add Sprite class

--- client/ts/gfx/Scene.ts
+++ client/ts/gfx/Scene.ts
@@ -1,5 +1,13 @@
+import {Sprite} from "./Sprite";
+
 export class Scene {
+    sprites: Sprite[] = []
+
     render(): void {
         // TODO
+    }
+
+    addSprite(spr: Sprite): void {
+        this.sprites.push(spr)
     }
 }
