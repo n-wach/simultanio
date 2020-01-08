e960672005f18eca2f0c1e2e5cffa303a160ebfe
n-wach <nmwachholz@ucsb.edu>
2020-01-08 14:56:49 -0800

    Flask server now serves client index.html and js

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,5 @@
-from app import app
+from server.app import app
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
-
+    app.run(debug=True, use_reloader=False)
 
--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, render_template, send_file
 
-app = Flask(__name__, static_url_path='', static_folder='static')
+app = Flask(__name__, static_url_path='', static_folder='../client/dist', template_folder="../client/templates")
 
 app.secret_key = os.environ.get("SECRET_KEY")
 if os.environ.get("SERVER_NAME") is not None:
     app.config["SERVER_NAME"] = os.environ.get("SERVER_NAME")
 
-@app.route("/", methods=["GET"])
-def hello():
-    return render_template("../client/index.html")
 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/js/bundle.js")
+def js_bundle():
+    return send_file("../client/dist/bundle.js")
