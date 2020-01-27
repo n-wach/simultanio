e383870750b7173de96ebc5a58c41a8e09bcf343
xdrie <xdrie@alt.icu>
2020-01-27 12:18:28 -0800

    reformat

--- client/ts/scenes/PlayScene.ts
+++ client/ts/scenes/PlayScene.ts
@@ -1,19 +1,19 @@
-import {Scene} from "../gfx/Scene";
-import {Button} from "../gfx/ui/Button";
-import {LobbyScene} from "./LobbyScene";
-import {Game} from "../gfx/Game";
-import {HUD} from "../game/HUD";
-import {Entity, EntityVariation, Match, PlayerCommand, TerrainTile} from "../comms";
-import {RenderableGroup} from "../gfx/RenderableGroup";
-import {Vec2} from "../gfx/Vec2";
-import {Renderable} from "../gfx/Renderable";
+import { Scene } from "../gfx/Scene";
+import { Button } from "../gfx/ui/Button";
+import { LobbyScene } from "./LobbyScene";
+import { Game } from "../gfx/Game";
+import { HUD } from "../game/HUD";
+import { Entity, EntityVariation, Match, PlayerCommand, TerrainTile } from "../comms";
+import { RenderableGroup } from "../gfx/RenderableGroup";
+import { Vec2 } from "../gfx/Vec2";
+import { Renderable } from "../gfx/Renderable";
 
 export class PlayScene extends Scene {
     initialize() {
         this.ui = new RenderableGroup(new HUD(this),
             new Button("Leave", 5, 5, 100, 20, () => {
-            Game.socketio.emit("leave match");
-        }));
+                Game.socketio.emit("leave match");
+            }));
         this.stage = new GameRenderable();
         Game.socketio.on("game update", (match: Match) => {
             Game.match = match;
@@ -30,7 +30,6 @@ export class PlayScene extends Scene {
 }
 
 class GameRenderable extends RenderableGroup {
-
     constructor() {
         super();
         Game.input.addHandler((event) => {
@@ -63,10 +62,10 @@ class GameRenderable extends RenderableGroup {
         ctx.strokeStyle = "#888888";
         ctx.lineWidth = 10;
         ctx.strokeRect(-5, -5, Game.match.terrain_view.width * 10 + 10, Game.match.terrain_view.height * 10 + 10);
-        for(let x = 0; x < Game.match.terrain_view.width; x++) {
-            for(let y = 0; y < Game.match.terrain_view.height; y++) {
+        for (let x = 0; x < Game.match.terrain_view.width; x++) {
+            for (let y = 0; y < Game.match.terrain_view.height; y++) {
                 let tile = Game.match.terrain_view.grid[x][y];
-                switch(tile) {
+                switch (tile) {
                     case TerrainTile.LAND:
                         ctx.fillStyle = "white";
                         break;
@@ -86,7 +85,7 @@ class GameRenderable extends RenderableGroup {
 
         ctx.fillStyle = Game.match.you.color;
         GameRenderable.drawEntities(ctx, Game.match.you.entities);
-        for(let player of Game.match.other_players) {
+        for (let player of Game.match.other_players) {
             ctx.fillStyle = player.color;
             GameRenderable.drawEntities(ctx, player.entities);
         }
@@ -94,10 +93,10 @@ class GameRenderable extends RenderableGroup {
     }
 
     static drawEntities(ctx: CanvasRenderingContext2D, entities: Entity[]) {
-        for(let entity of entities) {
+        for (let entity of entities) {
             ctx.moveTo(entity.x, entity.y);
             ctx.beginPath();
-            switch(entity.variation) {
+            switch (entity.variation) {
                 case EntityVariation.UNKNOWN:
                     console.error("tried to draw unknown entity:", entity);
                     break;
