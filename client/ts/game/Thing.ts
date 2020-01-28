a87573a724c04ea1046966dd1e0adac657d22394
n-wach <nmwachholz@gmail.com>
2020-01-28 01:41:42 -0800

    fix merge

--- client/ts/game/Thing.ts
+++ client/ts/game/Thing.ts
@@ -1,5 +1,6 @@
 import { Color, Id } from "../comms";
 import { Vec2 } from "../gfx/Vec2";
+import {Sprite} from "../gfx/Sprite";
 
 // represents a drawable game unit of some sort
 export class Thing extends Sprite {
