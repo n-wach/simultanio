00c7a35614df82a705b6e0176599c972ed8a9b40
n-wach <nmwachholz@gmail.com>
2020-01-28 01:37:49 -0800

    Rounded corners of terrain--look into performance implications

--- client/ts/scenes/PlayScene.ts
+++ client/ts/scenes/PlayScene.ts
@@ -39,13 +39,15 @@ class TerrainRenderable extends Renderable {
     
     render(ctx: CanvasRenderingContext2D): void {
         let s = TerrainRenderable.GRID_CELL_SIZE;
+        let c = s * 0.2;
         let hs = s / 2;
+        let w = Game.match.terrain_view.width;
+        let h = Game.match.terrain_view.height;
+        let g = Game.match.terrain_view.grid;
 
         //fill fog first
         ctx.fillStyle = Res.col_fog;
-        ctx.fillRect(0, 0,
-            Game.match.terrain_view.width * s,
-            Game.match.terrain_view.height * s);
+        ctx.fillRect(0, 0, w * s, h * s);
 
         /*
            TODO so this kinda speeds things up, but it's nowhere near fast enough for a large,
@@ -62,11 +64,95 @@ class TerrainRenderable extends Renderable {
             if(t == TerrainTile.WATER) ctx.fillStyle = Res.col_water;
             if(t == TerrainTile.MATTER_SOURCE) ctx.fillStyle = Res.col_matter;
             ctx.beginPath();
-            for (let x = 0; x < Game.match.terrain_view.width; x++) {
-                for (let y = 0; y < Game.match.terrain_view.height; y++) {
-                    let tile = Game.match.terrain_view.grid[x][y];
+            for (let x = 0; x < w; x++) {
+                for (let y = 0; y < h; y++) {
+                    let tile = g[x][y];
                     if(tile == t) {
-                        ctx.rect(x * s, y * s, s * 1.2, s * 1.2);
+                        ctx.rect(x * s, y * s, s, s);
+                    } else {
+                        let left = t;
+                        let right = t;
+                        let top = t;
+                        let bot = t;
+                        if (x > 0) left = g[x - 1][y];
+                        if (x < w - 1) right = g[x + 1][y];
+                        if (y > 0) top = g[x][y - 1];
+                        if (y < h - 1) bot = g[x][y + 1];
+
+                        if(left == t && top == t) {
+                            ctx.moveTo(x * s, y * s);
+                            ctx.lineTo(x * s + c, y * s);
+                            ctx.lineTo(x * s, y * s + c);
+                            ctx.closePath();
+                        }
+                        if(top == t && right == t) {
+                            ctx.moveTo((x + 1) * s, y * s);
+                            ctx.lineTo((x + 1) * s - c, y * s);
+                            ctx.lineTo((x + 1) * s, y * s + c);
+                            ctx.closePath();
+                        }
+                        if(right == t && bot == t) {
+                            ctx.moveTo((x + 1) * s, (y + 1) * s);
+                            ctx.lineTo((x + 1) * s - c, (y + 1) * s);
+                            ctx.lineTo((x + 1) * s, (y + 1) * s - c);
+                            ctx.closePath();
+                        }
+                        if(bot == t && left == t) {
+                            ctx.moveTo(x * s, (y + 1) * s);
+                            ctx.lineTo(x * s + c, (y + 1) * s);
+                            ctx.lineTo(x * s, (y + 1) * s - c);
+                            ctx.closePath();
+                        }
+                    }
+                }
+            }
+            ctx.fill();
+        }
+
+        for(let v in TerrainTile) {
+            let t = TerrainTile[v];
+            if(t == TerrainTile.UNKNOWN) ctx.fillStyle = Res.col_fog;
+            if(t == TerrainTile.LAND) ctx.fillStyle = Res.col_land;
+            if(t == TerrainTile.WATER) ctx.fillStyle = Res.col_water;
+            if(t == TerrainTile.MATTER_SOURCE) ctx.fillStyle = Res.col_matter;
+            ctx.beginPath();
+            for (let x = 0; x < w; x++) {
+                for (let y = 0; y < h; y++) {
+                    let tile = g[x][y];
+                    if(tile != t) {
+                        let left = t;
+                        let right = t;
+                        let top = t;
+                        let bot = t;
+                        if (x > 0) left = g[x - 1][y];
+                        if (x < w - 1) right = g[x + 1][y];
+                        if (y > 0) top = g[x][y - 1];
+                        if (y < h - 1) bot = g[x][y + 1];
+
+                        if(left == t && top == t) {
+                            ctx.moveTo(x * s, y * s);
+                            ctx.lineTo(x * s + c, y * s);
+                            ctx.lineTo(x * s, y * s + c);
+                            ctx.closePath();
+                        }
+                        if(top == t && right == t) {
+                            ctx.moveTo((x + 1) * s, y * s);
+                            ctx.lineTo((x + 1) * s - c, y * s);
+                            ctx.lineTo((x + 1) * s, y * s + c);
+                            ctx.closePath();
+                        }
+                        if(right == t && bot == t) {
+                            ctx.moveTo((x + 1) * s, (y + 1) * s);
+                            ctx.lineTo((x + 1) * s - c, (y + 1) * s);
+                            ctx.lineTo((x + 1) * s, (y + 1) * s - c);
+                            ctx.closePath();
+                        }
+                        if(bot == t && left == t) {
+                            ctx.moveTo(x * s, (y + 1) * s);
+                            ctx.lineTo(x * s + c, (y + 1) * s);
+                            ctx.lineTo(x * s, (y + 1) * s - c);
+                            ctx.closePath();
+                        }
                     }
                 }
             }
