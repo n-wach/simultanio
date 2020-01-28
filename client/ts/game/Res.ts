b013b218212d8cea7ccf68723ad328fee86c4fe8
xdrie <xdrie@alt.icu>
2020-01-28 10:54:11 -0800

    update color res

--- client/ts/game/Res.ts
+++ client/ts/game/Res.ts
@@ -1,16 +1,21 @@
 export class Res {
+    // map
     public static col_land :string = "#acbd95";
     public static col_water :string = "#7ca19f";
     public static col_matter :string = "#cfb494";
     public static col_fog :string = "#ada49c";
 
+    // ui
     public static col_bg :string = "#cfc5ba";
     public static col_uifg :string = "#f0e4f7";
     public static col_uifg_accent :string = "#d2b0e8";
     public static col_uibg :string = "#526266";
     public static col_uibg_accent :string = "#7a8d91";
     
+    // color palette
+    public static pal_BLACK :string = "#111111";
 
+    // fonts
     public static font_ui :string = "16px Share Tech Mono";
     public static font_ui_lg :string = "24px Share Tech Mono";
 
